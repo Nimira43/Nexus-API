27a06e5450c1ddad9f508cbf6810135e2f61d950
Nimira43 <n.rabone72@aol.com>
2025-01-17 06:03:10 +0000

    Deleted user.controller.ts and used Nest CLI to generate it again. This time a users.controller.spec.ts is also created.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common'
 import { UsersController } from './users.controller'
 
+
 @Module({
-  controllers: [
-    UsersController
-  ]
+  controllers: [  
+  UsersController]
 })
 export class UsersModule {}
