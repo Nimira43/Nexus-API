d595fac0477d53d5cd7c1a43aa54e8cbd58f46bd
Nimira43 <n.rabone72@aol.com>
2025-03-20 20:50:48 +0000

    Using IsEmail decorator for the email property instead of string

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator'
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator'
 
 export class CreateUserDto { 
   @IsString()
@@ -9,7 +9,7 @@ export class CreateUserDto {
   @IsOptional()
   lastName?: string
   
-  @IsString()
+  @IsEmail()
   @IsNotEmpty()
   email: string
   
