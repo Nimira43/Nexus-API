76cf35e7dc69dc84cc078d6e005422da264bef50
Nimira43 <n.rabone72@aol.com>
2025-03-20 20:53:15 +0000

    Added MinLength and MaxLength to the name properties

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,12 +1,16 @@
-import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator'
+import { IsEmail, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from 'class-validator'
 
 export class CreateUserDto { 
   @IsString()
   @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(96)
   firstName: string
 
   @IsString()
   @IsOptional()
+  @MinLength(3)
+  @MaxLength(96)
   lastName?: string
   
   @IsEmail()
