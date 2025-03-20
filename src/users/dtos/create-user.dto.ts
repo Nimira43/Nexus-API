179ca33e2abf8aacb81aac298bfc18a3fa10009c
Nimira43 <n.rabone72@aol.com>
2025-03-20 20:47:33 +0000

    Added IsNotEmpty decorator

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,13 +1,19 @@
-import { IsString } from 'class-validator'
+import { IsNotEmpty, IsString } from 'class-validator'
 
 export class CreateUserDto { 
   @IsString()
+  @IsNotEmpty()
   firstName: string
+
   @IsString()
   lastName: string
+  
   @IsString()
+  @IsNotEmpty()
   email: string
+  
   @IsString()
+  @IsNotEmpty()
   password: string
 }
 
