ff8dfe76ddc18cc612512410851136b11f910aeb
Nimira43 <n.rabone72@aol.com>
2025-07-21 18:24:56 +0100

    Import forwardRef in auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common'
+import { Module, forwardRef } from '@nestjs/common'
 import { AuthController } from './auth.controller'
 import { AuthService } from './providers/auth.service';
 import { UsersModule } from 'src/users/users.module';
