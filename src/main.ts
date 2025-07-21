bcc4a26005c8df4d974d47e57d52fe12fa012a20
Nimira43 <n.rabone72@aol.com>
2025-07-21 20:13:44 +0100

    Setting  up document path

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,19 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true, 
-  }))
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true, 
+    })
+  )
 
   const config = new DocumentBuilder().setVersion('1.0').build()
   const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
 
   await app.listen(process.env.PORT ?? 3000)
 }
+
 bootstrap()
