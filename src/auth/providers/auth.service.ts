e72b0f49e9c2448aea0b3dfeb4fd4c989ea6f1e2
Nimira43 <n.rabone72@aol.com>
2025-07-21 08:59:39 +0100

    Import UsersModule into auth module

--- src/auth/providers/auth.service.ts
+++ src/auth/providers/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class AuthService {
-  public login(email: string, password: string) {
+  public login(email: string, password: string, id: string) {
     
   }
 }
