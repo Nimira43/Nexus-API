691426bd9c9b9b57afb94211f7a8d3fba127bd63
Nimira43 <n.rabone72@aol.com>
2025-07-21 18:20:16 +0100

    Export AuthService in auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { UsersModule } from 'src/users/users.module';
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [UsersModule]
+  imports: [UsersModule],
+  exports: [AuthService],
 })
 export class AuthModule {}
