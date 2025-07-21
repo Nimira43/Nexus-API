478114642ab3178bbdbccfe22a439a44fc7cfbb5
Nimira43 <n.rabone72@aol.com>
2025-07-21 09:01:45 +0100

    Injecting UsersService

--- src/auth/providers/auth.service.ts
+++ src/auth/providers/auth.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common'
+import { UsersService } from 'src/users/providers/users.service';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private readonly usersService: UsersService
+  ) {}
+
   public login(email: string, password: string, id: string) {
-    
+
   }
 }
