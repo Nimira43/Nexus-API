fdc679f53feb5489af92526e4415fbc7049cd328
Nimira43 <n.rabone72@aol.com>
2025-08-04 21:47:37 +0100

    example value

--- src/users/dtos/get-users-param.dto.ts
+++ src/users/dtos/get-users-param.dto.ts
@@ -5,6 +5,7 @@ import { ApiPropertyOptional } from '@nestjs/swagger'
 export class GetUsersParamDto {
   @ApiPropertyOptional({
     description: 'Get user with a specific id',
+    example: 1234
   })
   @IsOptional()
   @IsInt()
