1a3e800d9d625c4853ce12aeaaf46b359f7c54cc
Nimira43 <n.rabone72@aol.com>
2025-08-12 12:19:45 +0100

    Setting status to use postStatus enum

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -1,8 +1,9 @@
+import { postStatus } from '../enums/postStatus.enum'
 import { postType } from '../enums/postType.enum'
 
 export class CreatePostDto {
   title: string
   postType: postType
   slug: string
-  status: string
+  status: postStatus
 }
