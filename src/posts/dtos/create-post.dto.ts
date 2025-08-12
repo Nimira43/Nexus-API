e647b100fe8fcaff2bbc6ac580beff1ea82e18dc
Nimira43 <n.rabone72@aol.com>
2025-08-12 12:26:37 +0100

    Added IsString decorator to title

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -1,8 +1,11 @@
+import { IsString } from 'class-validator'
 import { postStatus } from '../enums/postStatus.enum'
 import { postType } from '../enums/postType.enum'
 
 export class CreatePostDto {
+  @IsString()
   title: string
+  
   postType: postType
   slug: string
   status: postStatus
