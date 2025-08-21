c7a25fcf7f1b58aafa9006eeac72b818acb15674
Nimira43 <n.rabone72@aol.com>
2025-08-21 13:55:44 +0100

    Importing PartialType from Next JS / Swagger

--- src/posts/dtos/patch-post.dto.ts
+++ src/posts/dtos/patch-post.dto.ts
@@ -1,5 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types'
-import { ApiProperty } from '@nestjs/swagger'
+import { ApiProperty, PartialType } from '@nestjs/swagger'
 import { IsInt, IsNotEmpty } from 'class-validator'
 import { CreatePostDto } from './create-post.dto'
 
