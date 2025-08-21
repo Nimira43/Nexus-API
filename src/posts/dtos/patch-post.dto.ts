931a9ba629ca570ef606e0c42ff3c57ac388ca88
Nimira43 <n.rabone72@aol.com>
2025-08-21 13:46:39 +0100

    Added description toApiProperty

--- src/posts/dtos/patch-post.dto.ts
+++ src/posts/dtos/patch-post.dto.ts
@@ -2,7 +2,9 @@ import { ApiProperty } from '@nestjs/swagger'
 import { IsInt, IsNotEmpty } from 'class-validator'
 
 export class PatchPostDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: 'The ID of the post that needs to be updated.',
+  })
   @IsInt()
   @IsNotEmpty()
   id: number;
