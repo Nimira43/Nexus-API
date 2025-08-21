3129ee64274b0abfb6b1f0f73de1e722cf958354
Nimira43 <n.rabone72@aol.com>
2025-08-21 13:48:38 +0100

    Past in CreatePostDto as arguement

--- src/posts/dtos/patch-post.dto.ts
+++ src/posts/dtos/patch-post.dto.ts
@@ -1,8 +1,9 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/mapped-types'
 import { ApiProperty } from '@nestjs/swagger'
 import { IsInt, IsNotEmpty } from 'class-validator'
+import { CreatePostDto } from './create-post.dto'
 
-export class PatchPostDto extends PartialType(){
+export class PatchPostDto extends PartialType(CreatePostDto){
   @ApiProperty({
     description: 'The ID of the post that needs to be updated.',
   })
