4dd5f9748dc149d1dbd92d0ceecf5a9e00ee33ed
Nimira43 <n.rabone72@aol.com>
2025-08-21 13:47:40 +0100

    Using PartialType

--- src/posts/dtos/patch-post.dto.ts
+++ src/posts/dtos/patch-post.dto.ts
@@ -1,7 +1,8 @@
+import { PartialType } from '@nestjs/mapped-types';
 import { ApiProperty } from '@nestjs/swagger'
 import { IsInt, IsNotEmpty } from 'class-validator'
 
-export class PatchPostDto {
+export class PatchPostDto extends PartialType(){
   @ApiProperty({
     description: 'The ID of the post that needs to be updated.',
   })
