295251a353ff429792ec296d1fd0ca92eba74ec8
Nimira43 <n.rabone72@aol.com>
2025-08-21 13:45:32 +0100

    Added decorators

--- src/posts/dtos/patch-post.dto.ts
+++ src/posts/dtos/patch-post.dto.ts
@@ -1,3 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { IsInt, IsNotEmpty } from 'class-validator'
+
 export class PatchPostDto {
+  @ApiProperty()
+  @IsInt()
+  @IsNotEmpty()
   id: number;
 }
