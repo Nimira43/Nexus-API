bfb683309fe28f6305b3868ab2a61db86ecd09a6
Nimira43 <n.rabone72@aol.com>
2025-08-21 15:11:02 +0100

    Writing comments for the Compodocs docs. Also commenting out the UsersServices constructor and AuthService set up for findAll

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,21 +1,26 @@
 import { Injectable, forwardRef, Inject } from '@nestjs/common'
 import { GetUsersParamDto } from '../dtos/get-users-param.dto'
-import { AuthService } from 'src/auth/providers/auth.service'
+// import { AuthService } from 'src/auth/providers/auth.service'
 
+/**
+ * Class to connect to Users table and perform business operations
+ */
 @Injectable()
 export class UsersService{
-  constructor(
-    @Inject(forwardRef(() => AuthService))
-    private readonly authService: AuthService
-  ) {}
-
+  // constructor(
+  //   @Inject(forwardRef(() => AuthService))
+  //   private readonly authService: AuthService
+  // ) {}
+  /**
+   * The method to get all the users from the database
+   */
   public findAll(
     getUserParamDto: GetUsersParamDto,
     limit: number, 
     page: number,
   ) {
-    const isAuth = this.authService.isAuth()
-    console.log(isAuth)
+    // const isAuth = this.authService.isAuth()
+    // console.log(isAuth)
 
     return [
       {
@@ -28,7 +33,9 @@ export class UsersService{
       },
     ]
   }
-
+  /**
+     * Finds a single user using the ID of the user
+     */
   public findOneById(id: string) { 
     return {
       id: 1234,
