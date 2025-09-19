1f1d39c6ba8b3df518382e502feade2d18cd54c5
Nimira43 <n.rabone72@aol.com>
2025-09-19 20:40:21 +0100

    Bringing in Column decorator

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,8 +1,9 @@
-import { Entity } from 'typeorm'
+import { Column, Entity } from 'typeorm'
 
 @Entity()
 export class User {
   id: number
+  @Column()
   firstName: string
   lastName: string
   email: string
