c10073881ccbc0e872e6b75f2365dadcb39e7298
Nimira43 <n.rabone72@aol.com>
2025-09-24 19:20:33 +0100

    Added decorators to name.

--- src/tags/dtos/create-tag.dto.ts
+++ src/tags/dtos/create-tag.dto.ts
@@ -1,10 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator'
+
 export class CreateTagDto {
+  @ApiProperty()
+  @IsString()
+  @MinLength(3)
+  @IsNotEmpty()
+  @MaxLength(256)
   name: string
+
+  
   slug: string
+  
+  
   description?: string
+  
+  
   schema?: string
+  
+  
   featuredImageUrl?: string
-  createDate: Date
-  updateDate: Date
-  deletedAt: Date
 }
