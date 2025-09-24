0b63525d5bedd5605dd522caa5cdb268f3aa833c
Nimira43 <n.rabone72@aol.com>
2025-09-24 17:20:04 +0100

    Added slug and postStatus. Imported postStatus enum

--- src/posts/post.entity.ts
+++ src/posts/post.entity.ts
@@ -1,9 +1,14 @@
 import { Entity } from 'typeorm'
 import { postType } from './enums/postType.enum'
+import { postStatus } from './enums/postStatus.enum'
 
 @Entity()
 export class Post {
   title: string
 
   postType: postType
+
+  slug: string
+
+  status: postStatus
 }
