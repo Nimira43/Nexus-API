69be19a4223b377decec37abf2ac05ad47b9968c
Nimira43 <n.rabone72@aol.com>
2025-09-24 18:31:56 +0100

    Applying Column decorators - 1

--- src/tags/tag.entity.ts
+++ src/tags/tag.entity.ts
@@ -1,15 +1,49 @@
-import { Entity } from 'typeorm'
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 
 @Entity()
 export class Tag {
+
+  @PrimaryGeneratedColumn()
   id: number
+
+  @Column({
+    type: 'varchar',
+    length: 256,
+    nullable: false,
+    unique: true,
+  })
   name: string
+  
+  @Column({
+    type: 'varchar',
+    length: 512,
+    nullable: false,
+    unique: true,
+  })
   slug: string
+  
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
   description: string
+  
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
   schema: string
+  
+  
   featuredImageUrl: string
+  
+  
   createDate: Date
+  
+  
   updateDate: Date
+  
+  
   deletedAt: Date
 }
 
