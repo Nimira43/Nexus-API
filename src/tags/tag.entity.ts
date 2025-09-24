41fdf3405cbc27d060cb09455d89514199099f82
Nimira43 <n.rabone72@aol.com>
2025-09-24 18:24:46 +0100

    Created properties for Tag

--- src/tags/tag.entity.ts
+++ src/tags/tag.entity.ts
@@ -1,5 +1,12 @@
 import { Entity } from 'typeorm'
 
 @Entity()
-export class Tag {}
+export class Tag {
+  id: number
+  name: string
+  slug: string
+  description: string
+  schema: string
+  featuredImageUrl: string
+}
 
