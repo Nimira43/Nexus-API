2f79a490aee84f24e9c85d725ee49c881aa50be5
Nimira43 <n.rabone72@aol.com>
2025-09-24 18:37:11 +0100

    Added DeleteDateColumn decorator to deletedAt

--- src/tags/tag.entity.ts
+++ src/tags/tag.entity.ts
@@ -1,4 +1,4 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm'
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm'
 
 @Entity()
 export class Tag {
@@ -47,7 +47,7 @@ export class Tag {
   @UpdateDateColumn()
   updateDate: Date
   
-  
+  @DeleteDateColumn()
   deletedAt: Date
 }
 
