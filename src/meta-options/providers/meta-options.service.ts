43f4814992af2f3bc4892308d916cd7a50cc3e3a
Nimira43 <n.rabone72@aol.com>
2025-09-29 15:24:24 +0100

    Takes in MetaOption as an argument.

--- src/meta-options/providers/meta-options.service.ts
+++ src/meta-options/providers/meta-options.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common'
 import { CreatePostMetaOptionsDto } from '../dtos/create-post-meta-options.dto'
 import { Repository } from 'typeorm'
+import { MetaOption } from '../meta-option.entity'
 
 @Injectable()
 export class MetaOptionsService {
 
   constructor(
-    private readonly metaOptionsRepository: Repository,
+    private readonly metaOptionsRepository: Repository<MetaOption>,
   ) {}
 
   public async create(createPostMetaOptionsDto: CreatePostMetaOptionsDto) {}
