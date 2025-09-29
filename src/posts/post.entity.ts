54e666bd1db30fab05e9394a9a6cf00450335c5a
Nimira43 <n.rabone72@aol.com>
2025-09-29 15:08:14 +0100

    In post.entity.ts - added OneToOne decorator to metaOptions - linked to MetaOption. Changed type to MetaOption

--- src/posts/post.entity.ts
+++ src/posts/post.entity.ts
@@ -1,7 +1,8 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm'
 import { postType } from './enums/postType.enum'
 import { postStatus } from './enums/postStatus.enum'
 import { CreatePostMetaOptionsDto } from '../meta-options/dtos/create-post-meta-options.dto'
+import { MetaOption } from 'src/meta-options/meta-option.entity'
 
 @Entity()
 export class Post {
@@ -64,8 +65,10 @@ export class Post {
     nullable: true
   })
   publishOn?: Date
-  
+
+  @OneToOne(() => MetaOption)
+  metaOptions?: MetaOption
   
   tags?: string[]
-  metaOptions?: CreatePostMetaOptionsDto[]
+  
 }
