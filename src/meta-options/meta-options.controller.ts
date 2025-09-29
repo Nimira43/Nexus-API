11a2211c989cca7c70e32f75430c334482c45da3
Nimira43 <n.rabone72@aol.com>
2025-09-29 15:40:10 +0100

    Using create in MetaOptionsController - 1

--- src/meta-options/meta-options.controller.ts
+++ src/meta-options/meta-options.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Post } from '@nestjs/common'
+import { CreatePostMetaOptionsDto } from './dtos/create-post-meta-options.dto'
 
 @Controller('meta-options')
-export class MetaOptionsController {}
+export class MetaOptionsController {
+  @Post()
+  public create(@Body() createPostMetaOptionsDto: CreatePostMetaOptionsDto) {
+
+  }
+}
