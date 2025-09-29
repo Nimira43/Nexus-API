c09b3f543c37789f664f1eb5527cd03f2f01271a
Nimira43 <n.rabone72@aol.com>
2025-09-29 15:23:30 +0100

    Created constructor. Inject metaOptionsRepository

--- src/meta-options/providers/meta-options.service.ts
+++ src/meta-options/providers/meta-options.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@nestjs/common'
 import { CreatePostMetaOptionsDto } from '../dtos/create-post-meta-options.dto'
+import { Repository } from 'typeorm'
 
 @Injectable()
 export class MetaOptionsService {
+
+  constructor(
+    private readonly metaOptionsRepository: Repository,
+  ) {}
+
   public async create(createPostMetaOptionsDto: CreatePostMetaOptionsDto) {}
 }
